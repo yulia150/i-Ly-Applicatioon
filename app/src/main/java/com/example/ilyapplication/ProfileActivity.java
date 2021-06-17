a8b91776ea1c437cd606975a6195eff114670f13
yulia150 <yuliyastutik1@gmail.com>
2021-06-17 12:36:37 +0700

    SignIn and Sign Up design

--- app/src/main/java/com/example/ilyapplication/ProfileActivity.java
+++ app/src/main/java/com/example/ilyapplication/ProfileActivity.java
@@ -2,13 +2,32 @@ package com.example.ilyapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_back:
+                Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
+                startActivity(intent);
+
+        }
+
     }
 }
